96bd42b6184380ac69407c1072c6bff1ff506c00
Steven steven <Steven steven>
2016-10-29 14:22:11 +0800

    Initial commit

---
+++ src/com/jxy/dome/HelloWord.java
@@ -0,0 +1,10 @@
+package com.jxy.dome;
+
+/**
+ * Created by somea on 2016/10/29.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.print("HelloWord");
+    }
+}
